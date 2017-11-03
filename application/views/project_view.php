747c07d3e11ec92668eaadc5eed3e4c1574bfbf6
motherPENGUIN <gaylon_ian@yahoo.com.ph>
2017-11-04 00:42:23 +0800

    yah
    back

--- application/views/project_view.php
+++ application/views/project_view.php
@@ -47,6 +47,6 @@
 		doc.close();
 		doc.open();
 		doc.write(
-		result.innerHTML = '<?php' + PHP_input.value '?>' + '<style>' + CSS_input.value + '</style>' + '<script>' + JS_input.value + '</' + 'script>' );
+		result.innerHTML = PHP_input.value + '<style>' + CSS_input.value + '</style>' + '<script>' + JS_input.value + '</' + 'script>' );
 	})
 </script>
