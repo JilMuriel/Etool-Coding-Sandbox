e0ef03623aa2f505d3f960d5ec31dfaf854d1bfa
Vigil Muriel C. Boniol <vmuriel96@gmail.com>
2017-11-03 23:13:00 +0800

    code mirror

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,6 +1,15 @@
+<!-- jquery -->
 <script src="/assets/js/jquery-3.2.1.min.js"></script>
+<!-- popper -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+<!-- bootstrap -->
 <script src="/assets/js/bootstrap.min.js"></script>
-
+<!-- code mirror -->
+<script src="/assets/codemirror/lib/codemirror.css"></script>
+<script>
+  var editor = CodeMirror.fromTextArea(myTextarea, {
+    lineNumbers: true
+  });
+</script>
 </body>
 </html>
