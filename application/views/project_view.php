9907d04f433a6d101064a941a17b1fdf7de1760b
motherPENGUIN <gaylon_ian@yahoo.com.ph>
2017-11-06 01:46:02 +0800

    tapos
    na

--- application/views/project_view.php
+++ application/views/project_view.php
@@ -1,3 +1,14 @@
+<?php
+$html = null;
+$css = null;
+$js = null;
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST'  && !empty($_POST['pphhpp']) || $_SERVER['REQUEST_METHOD'] === 'POST'  && !empty($_POST['ccsssss']) || $_SERVER['REQUEST_METHOD'] === 'POST'  && !empty($_POST['jjss'])) {
+	$html = $_POST['pphhpp'];
+	$css = $_POST['ccsssss'];
+	$js = $_POST['jjss'];
+}
+?>
 <div class="Dashboard h-100">
 	<div class="d-flex flex-row">
 	  <div class="side-nav h-100 shadow">
@@ -6,16 +17,18 @@
 		</ul>
 	  </div>
 	  <div class="page-box pt-4 w-100">
-	  	<button class="btn btn-default float-right mr-4 mb-4" id="run_button">Run</button>
+	  	
 	  	<div class="ml-4 mr-4">
-	  		<iframe id="result" class="w-100" style="height: 500px;"></iframe>
+	  		<div id="result" class="w-100" style="height: 500px;"><?php echo $html . '<style>' . $css . '</style>' . '<script>' . $js . '</script>';?></div>
+	  		<form id="preview-form" method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+	  		<button type="submit" class="btn btn-default float-right mr-4 mb-4" id="run_button">Run</button>
 	  		<div class="row mt-4">
 	  			<div class="col-4">
 	  				<div class="card-ide shadow-super-light ">
 	  					<div class="p-2 header-ide text-light">
 	  						<h6>PHP</h6>
 		  				</div>
-		  				<div id="PHP_input" class=" w-100 border-0 text-ide" style="height: 250px"></div>
+		  				<textarea name="pphhpp" id="PHP_input" class="asd w-100 border-0 text-ide" style="height: 250px"></textarea>
 	  				</div>
 	  			</div>
 	  			<div class="col-4">
@@ -23,7 +36,7 @@
 	  					<div class="p-2 header-ide text-light">
 	  						<h6>CSS</h6>
 		  				</div>
-		  				<div id="CSS_input" class="w-100 border-0 text-ide" style="height: 250px"></div>
+		  				<textarea name="ccsssss" id="CSS_input" class="w-100 border-0 text-ide" style="height: 250px"></textarea>
 	  				</div>
 	  			</div>
 	  			<div class="col-4">
@@ -31,32 +44,30 @@
 	  					<div class="p-2 header-ide text-light">
 	  						<h6>Javascript</h6>
 		  				</div>
-		  				<div id="JS_input" class="w-100 border-0 text-ide" style="height: 250px"></div>
+		  				<textarea name="jjss" id="JS_input" class="w-100 border-0 text-ide" style="height: 250px"></textarea>
 	  				</div>
 	  			</div>
 	  		</div>
 	  	</div>
+	  	</form>
 	  </div>
 	</div>
 </div>
 <script type="text/javascript">
-	var doc = result.contentDocument;
 
-	run_button.addEventListener('click', function(){
-
-		doc.close();
-		doc.open();
-		doc.write(
-		result.innerHTML = PHP_input.value + '<style>' + CSS_input.value + '</style>' + '<script>' + JS_input.value + '</' + 'script>' );
-	});
-
-	var editor = CodeMirror(document.getElementById('JS_input'), {
-  mode:  "javascript"
+	var editor = CodeMirror.fromTextArea(document.getElementById('JS_input'), {
+  mode:  "javascript",
+  matchBrackets: true,
+  lineNumbers : true
 });
-	var myCodeMirror = CodeMirror(document.getElementById('PHP_input'), {
-  mode:  "xml"
+	var myCodeMirror = CodeMirror.fromTextArea(document.getElementById('PHP_input'), {
+  mode:  "xml",
+  matchBrackets: true,
+   lineNumbers : true
 });
-	var myCodeMirror1 = CodeMirror(document.getElementById('CSS_input'), {
-  mode:  "css"
+	var myCodeMirror1 = CodeMirror.fromTextArea(document.getElementById('CSS_input'), {
+  mode:  "css",
+  matchBrackets: true,
+   lineNumbers : true
 }); 
 </script>
