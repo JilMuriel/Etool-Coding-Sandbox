1a2af48ebe2ef34c6d97660d099cfa99e9ec4f7f
motherPENGUIN <gaylon_ian@yahoo.com.ph>
2017-11-06 03:11:42 +0800

    oks na
    oo

--- application/views/project_view.php
+++ application/views/project_view.php
@@ -1,14 +1,4 @@
-<?php
-$html = null;
-$css = null;
-$js = null;
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST'  && !empty($_POST['pphhpp']) || $_SERVER['REQUEST_METHOD'] === 'POST'  && !empty($_POST['ccsssss']) || $_SERVER['REQUEST_METHOD'] === 'POST'  && !empty($_POST['jjss'])) {
-	$html = $_POST['pphhpp'];
-	$css = $_POST['ccsssss'];
-	$js = $_POST['jjss'];
-}
-?>
 <div class="Dashboard h-100">
 	<div class="d-flex flex-row">
 	  <div class="side-nav h-100 shadow">
@@ -19,9 +9,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST'  && !empty($_POST['pphhpp']) || $_SERV
 	  <div class="page-box pt-4 w-100">
 	  	
 	  	<div class="ml-4 mr-4" style="padding-top: 100px;">
-	  		<iframe class="w-100" src="<?php echo $html?>"></iframe>
-	  		
-	  		<form id="preview-form" method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+	  		<iframe class="w-100" id="result">"></iframe>
 	  		<button type="submit" class="btn btn-default float-right mr-4 mb-4" id="run_button">Run</button>
 	  		<div class="row mt-4">
 	  			<div class="col-4">
@@ -29,7 +17,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST'  && !empty($_POST['pphhpp']) || $_SERV
 	  					<div class="p-2 header-ide text-light">
 	  						<h6>PHP</h6>
 		  				</div>
-		  				<textarea name="pphhpp" id="PHP_input" class="asd w-100 border-0 text-ide" style="height: 250px"></textarea>
+		  				<textarea name="pphhpp" id="phpbox" class="asd w-100 border-0 text-ide" style="height: 250px"></textarea>
 	  				</div>
 	  			</div>
 	  			<div class="col-4">
@@ -37,7 +25,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST'  && !empty($_POST['pphhpp']) || $_SERV
 	  					<div class="p-2 header-ide text-light">
 	  						<h6>CSS</h6>
 		  				</div>
-		  				<textarea name="ccsssss" id="CSS_input" class="w-100 border-0 text-ide" style="height: 250px"></textarea>
+		  				<textarea name="ccsssss" id="cssbox" class="w-100 border-0 text-ide" style="height: 250px"></textarea>
 	  				</div>
 	  			</div>
 	  			<div class="col-4">
@@ -45,30 +33,39 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST'  && !empty($_POST['pphhpp']) || $_SERV
 	  					<div class="p-2 header-ide text-light">
 	  						<h6>Javascript</h6>
 		  				</div>
-		  				<textarea name="jjss" id="JS_input" class="w-100 border-0 text-ide" style="height: 250px"></textarea>
+		  				<textarea name="jjss" id="jsbox" class="w-100 border-0 text-ide" style="height: 250px"></textarea>
 	  				</div>
 	  			</div>
 	  		</div>
 	  	</div>
-	  	</form>
 	  </div>
 	</div>
 </div>
 <script type="text/javascript">
+	var doc = result.contentDocument;
 
-	var editor = CodeMirror.fromTextArea(document.getElementById('JS_input'), {
+	run_button.addEventListener('click', function(){
+		doc.close();
+		doc.open();
+		doc.write(
+			myCodeMirror.getValue() + '<style>' + myCodeMirror1.getValue() + '</style>' + '<script>' + editor.getValue() + '</' + 'script>' )
+	});
+
+	
+
+	var editor = CodeMirror.fromTextArea(document.getElementById('jsbox'), {
   mode:  "javascript",
   matchBrackets: true,
-  lineNumbers : true
+  lineNumbers : true,
 });
-	var myCodeMirror = CodeMirror.fromTextArea(document.getElementById('PHP_input'), {
+	var myCodeMirror = CodeMirror.fromTextArea(document.getElementById('phpbox'), {
   mode:  "xml",
   matchBrackets: true,
-   lineNumbers : true
+   lineNumbers : true,
 });
-	var myCodeMirror1 = CodeMirror.fromTextArea(document.getElementById('CSS_input'), {
+	var myCodeMirror1 = CodeMirror.fromTextArea(document.getElementById('cssbox'), {
   mode:  "css",
   matchBrackets: true,
-   lineNumbers : true
+   lineNumbers : true,
 }); 
 </script>
