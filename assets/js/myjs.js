ef0136aa8381aa581bd402d663f1fc16c54694d7
Vigil Muriel C. Boniol <vmuriel96@gmail.com>
2017-11-09 20:23:11 +0800

    update

--- assets/js/myjs.js
+++ assets/js/myjs.js
@@ -5,3 +5,30 @@ $('#myTab a').on('click', function (e) {
 $(function () {
   $('[data-toggle="popover"]').popover()
 })
+//code editor js
+var doc = result.contentDocument;
+
+	run_button.addEventListener('click', function(){
+		doc.close();
+		doc.open();
+		doc.write(
+			myCodeMirror.getValue() + '<style>' + myCodeMirror1.getValue() + '</style>' + '<script>' + editor.getValue() + '</' + 'script>' );
+	});
+
+	
+
+	var editor = CodeMirror.fromTextArea(document.getElementById('jsbox'), {
+  mode:  "javascript",
+  matchBrackets: true,
+  lineNumbers : true,
+});
+	var myCodeMirror = CodeMirror.fromTextArea(document.getElementById('phpbox'), {
+  mode:  "xml",
+  matchBrackets: true,
+   lineNumbers : true,
+});
+	var myCodeMirror1 = CodeMirror.fromTextArea(document.getElementById('cssbox'), {
+  mode:  "css",
+  matchBrackets: true,
+   lineNumbers : true,
+}); 
