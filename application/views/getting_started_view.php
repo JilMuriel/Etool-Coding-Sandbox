8a3f955a4a5c7d73e93e527c58dbf61bc44bd334
Vigil Muriel C. Boniol <vmuriel96@gmail.com>
2017-11-09 23:51:53 +0800

    update

--- application/views/getting_started_view.php
+++ application/views/getting_started_view.php
@@ -7,37 +7,36 @@
 			    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
 			    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
 			  </ol>
+			  <h1>Guide1</h1>
+			  <hr>
 			  <div class="carousel-inner  pb-4 mb-4">
 			    <div class="carousel-item active">
-			      <h1>Guide1</h1>
 			      <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 			      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 			      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 			      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
 			      cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
 			      proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-			      <a class="btn btn-primary float-right" data-target="#carouselExampleIndicators" data-slide-to="1">next</a>
+			     <!--  <a class="btn btn-primary float-right" data-target="#carouselExampleIndicators" data-slide-to="1">next</a> -->
 			    </div>
 			    <div class="carousel-item">
-			      <h1>Guide2</h1>
 			       <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 			      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 			      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 			      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
 			      cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
 			      proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-			      <a class="btn btn-primary" data-target="#carouselExampleIndicators" data-slide-to="0">next</a>
-			      <a class="btn btn-primary float-right" data-target="#carouselExampleIndicators" data-slide-to="1">next</a>
+			      <!-- <a class="btn btn-primary" data-target="#carouselExampleIndicators" data-slide-to="0">next</a>
+			      <a class="btn btn-primary float-right" data-target="#carouselExampleIndicators" data-slide-to="1">next</a> -->
 			    </div>
 			    <div class="carousel-item">
-			      <h1>Guide3</h1>
 			       <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 			      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 			      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 			      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
 			      cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
 			      proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-			      <a class="btn btn-primary float-right" data-target="#carouselExampleIndicators" data-slide-to="1">next</a>
+			     <!--  <a class="btn btn-primary float-right" data-target="#carouselExampleIndicators" data-slide-to="1">next</a> -->
 			    </div>
 			  </div>
 
