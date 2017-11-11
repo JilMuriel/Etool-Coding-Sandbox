49648bc201db12b802a9f2577d89b25c26e0bffc
Vigil Muriel C. Boniol <vmuriel96@gmail.com>
2017-11-12 03:42:11 +0800

    update

--- application/views/admin_profile_viewer.php
+++ application/views/admin_profile_viewer.php
@@ -1,3 +1,30 @@
 <div class="User_profile mt-5 pt-3">
-	<h1>Hello World</h1>
+	<div class="container-fluid">
+		<div>
+			<h3 class="accent">User Info</h3>
+			<div></div>
+			<div></div>
+		</div>
+		<div class="users-project  ml-4 mr-4">
+			<table class="table table-hover shadow-super-light">
+			  <thead>
+			    <tr>
+			      <th scope="col"><small class="accent">Name</small></th>
+			      <th scope="col"><small class="accent">Database</small></th>
+			      <th scope="col"><small class="accent">Date Created</small></th>
+			      <th scope="col"><small class="accent">Last Update</small></th>
+			      <th scope="col"><i class="fa fa-bars accent" aria-hidden="true"></i></th>
+			    </tr>
+			  </thead>
+			  <tbody>
+			    <tr>
+			      <td>My Sample Project 1</td>
+			      <td>Le Database</td>
+			      <td>11/13/2017</td>
+			      <td>11/13/2017</td>
+			    </tr>
+			  </tbody>
+			</table>
+		</div>
+	</div>
 </div>
