1e8febfa89d29aad32afb3d94275bc2ebf82f459
Vigil Muriel C. Boniol <vmuriel96@gmail.com>
2017-11-11 22:09:32 +0800

    fix

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -54,45 +54,59 @@ class User extends CI_Controller {
     }
     public function validate_user() {
         $data['title'] = 'Validate ID';
+        $this->load->view('templates/header', $data);
+        $this->load->view('enter_id_view');
+        $this->load->view('templates/footer');
         $this->form_validation->set_rules('txtuserid', 'User ID','trim|required');
-        $session = array (
+
+        if ($this->form_validation->run() == FALSE) {
+            $this->load->view('templates/header', $data);
+            $this->load->view('enter_id_view');
+            $this->load->view('templates/footer');
+        } else {
+            $account_id = $this->security->xss_clean($this->input->post('txtuserid'));
+
+            $result = $this->user_model->id_verify($account_id);
+            if ($result) {
+                foreach($result as $row) {
+                    $session = array (
                     'user_id' => $row->account_id
                     );
                     redirect('user/create_account');
-                    // redirect('user/create_account');
-                    $data['title'] = 'create account';
-                    $data['id'] = $session['user_id'];
-                    echo $data['id'];
-                    echo $leId = $data['id'];
-                    echo $this->$la_id = $leId;
-                        $this->form_validation->set_rules('txtusername', 'Username', 'required');
-                        $this->form_validation->set_rules('txtpassword', 'Password', 'required');
-                        $this->form_validation->set_rules('txtemail', 'Email', 'required');
-                        if ($this->form_validation->run() == False) {
-                            $this->load->view('templates/header', $data);
-                            $this->load->view('create_account_view');
-                            $this->load->view('templates/footer');
-                        }
-                        else {
-                            $data = array(
-                                'username' => $this->input->post('txtusername'),
-                                'password' => $this->input->post('txtpassword'),
-                                'email' => $this->input->post('txtemail')
-                                // 'fname' => $this->input->post('txtfname'),
-                                // 'lname' => $this->input->post('txtlname')
-                            );
-                            $this->user_model->m_register($leId, $data);
-                            // $this->session->sess_destroy();
-                            redirect('user');
-                        }
-
                 }
+            }
+            else {
+                // $this->session->flashdata('user_registered');
+                redirect('user/registration_verification');
+            }
+        }
+
+    }
                 // echo $id = $session['user_id'];
     public function create_account() {
         $data['title'] = 'Create account';
-    	$this->load->view('templates/header', $data);
-    	$this->load->view('create_account_view');
-    	$this->load->view('templates/footer');
+        $this->form_validation->set_rules('txtusername', 'Username', 'required');
+        $this->form_validation->set_rules('txtpassword', 'Password', 'required');
+        $this->form_validation->set_rules('txtemail', 'Email', 'required');
+
+
+        if ($this->form_validation->run() == FALSE) {
+            $this->load->view('templates/header', $data);
+            $this->load->view('create_account_view');
+            $this->load->view('templates/footer');
+        }
+        else {
+            $data = array(
+                'username' => $this->input->post('txtusername'),
+                'password' => $this->input->post('txtpassword'),
+                'email' => $this->input->post('txtemail')
+                // 'fname' => $this->input->post('txtfname'),
+                // 'lname' => $this->input->post('txtlname')
+            );
+            $this->user_model->m_register($id, $data);
+            // $this->session->sess_destroy();
+            redirect('user');
+        }
     }
     public function logout() {
         $this->session->sess_destroy();
