d89d02820d52e76fa899ef61f01d3a2999cb4a1c
motherPENGUIN <gaylon_ian@yahoo.com.ph>
2017-11-13 06:35:28 +0800

    qwe
    qwe

--- application/views/creator_view.php
+++ application/views/creator_view.php
@@ -153,7 +153,7 @@
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-        <button type="button" class="btn btn-primary" id="clickhere">export</button>
+        <button type="button" class="btn btn-primary" id="clickhere">Export</button>
       </div>
     </div>
   </div>
